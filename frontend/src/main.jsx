06ae79e5501433f2994a07be6e48011f2ad4bc49
Gimhani <silvagsd@students.nsbm.ac.lk>
2026-03-17 18:00:23 +0530

    refactor: Replace BrowserRouter with createBrowserRouter and RouterProvider for improved routing structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  { path: '/*', element: <App /> },
+])
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
 )
